8e623b45afa1e467a58468c8e343f8c06784be36
haileyhoyat <whatuphails@yahoo.com>
2024-01-17 19:05:24 -0500

    cleanup indygo

--- city_scrapers/spiders/ind_indygo_gov_audit.py
+++ city_scrapers/spiders/ind_indygo_gov_audit.py
@@ -1,4 +1,3 @@
-from city_scrapers_core.constants import BOARD
 from city_scrapers_core.items import Meeting
 from city_scrapers_core.spiders import CityScrapersSpider
 from dateutil.parser import parser
@@ -9,6 +8,20 @@ class IndIndygoGovAuditSpider(CityScrapersSpider):
     agency = "Indianapolis Indygo Governance Audit Committee"
     timezone = "America/Detroit"
     start_urls = ["https://www.indygo.net/about-indygo/board-of-directors/"]
+    location = {
+        "name": "Administrative Office - Board Room",
+        "address": "1501 W. Washington St. Indianapolis, IN 46222",
+    }
+    links = [
+        {
+            "href": "https://www.indygo.net/about-indygo/board-of-directors/",
+            "title": "Meeting Page",
+        },
+        {
+            "href": "https://public.onboardmeetings.com/Group/HrdLpC4rmFdYrgplGJZm82TtkS14OCvw7QLcFFPpPrIA/TQhh8dVF3IFGmqPt9KPGgkEFwuhy9cHXFm5%2FFdjTKHsA",  # noqa
+            "title": "Past Governance Audit Committee packets",
+        },
+    ]
 
     def parse(self, response):
         """
@@ -29,15 +42,15 @@ class IndIndygoGovAuditSpider(CityScrapersSpider):
         for date_item in meeting_section.css("ul li"):
             meeting = Meeting(
                 title="IndyGo Governance and Audit Committee",
-                description=self._parse_description(date_item),
+                description="",
                 classification=None,
                 start=self._parse_start(date_item, meeting_year, meeting_time),
-                end=self._parse_end(date_item),
-                all_day=self._parse_all_day(date_item),
-                time_notes=self._parse_time_notes(date_item),
-                location=self._parse_location(date_item),
-                links=self._parse_links(date_item),
-                source=self._parse_source(response),
+                end=None,
+                all_day=False,
+                time_notes="",
+                location=self.location,
+                links=self.links,
+                source=response.url,
             )
 
             meeting["status"] = self._get_status(meeting)
@@ -45,56 +58,7 @@ class IndIndygoGovAuditSpider(CityScrapersSpider):
 
             yield meeting
 
-    def _parse_title(self, date_item):
-        title = "IndyGo Governance Audit Committee"
-        return title
-
-    def _parse_description(self, date_item):
-        """Parse or generate meeting description."""
-        description = ""
-        return description
-
-    def _parse_classification(self, date_item):
-        """Parse or generate classification from allowed options."""
-        return BOARD
-
     def _parse_start(self, date_item, meeting_year, meeting_time):
         """Parse start datetime as a naive datetime object."""
         meeting_date = (date_item.css("::text").get()).split("-")[0]
         return parser().parse(meeting_date + " " + meeting_year + " " + meeting_time)
-
-    def _parse_end(self, date_item):
-        """Parse end datetime as a naive datetime object. Added by pipeline if None"""
-        return None
-
-    def _parse_time_notes(self, date_item):
-        """Parse any additional notes on the timing of the meeting"""
-        return ""  # noqa
-
-    def _parse_all_day(self, date_item):
-        """Parse or generate all-day status. Defaults to False."""
-        return False
-
-    def _parse_location(self, date_item):
-        """Parse or generate location."""
-        return {
-            "address": "1501 W. Washington St. Indianapolis, IN 46222",
-            "name": "Administrative Office - Board Room",
-        }
-
-    def _parse_links(self, date_item):
-        """Parse or generate links."""
-        return [
-            {
-                "href": "https://www.indygo.net/about-indygo/board-of-directors/",
-                "title": "Meeting Page",
-            },
-            {
-                "href": "https://public.onboardmeetings.com/Group/HrdLpC4rmFdYrgplGJZm82TtkS14OCvw7QLcFFPpPrIA/TQhh8dVF3IFGmqPt9KPGgkEFwuhy9cHXFm5%2FFdjTKHsA",  # noqa
-                "title": "Past Governance Audit Committee packets",
-            },
-        ]
-
-    def _parse_source(self, response):
-        """Parse or generate source."""
-        return response.url
